6d41549a35e149cae7bdb819ad169734a6a942fd
teaca_daniele <teaca.daniele@iisgalvanimi.edu.it>
2023-05-24 11:00:24 +0000

    film

---
+++ templates_flask/app.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from flask import Flask
+import mysql.connector
+
+#Connect to mysql
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="ghiblidata"
+)
+mycursor = mydb.cursor()
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    mycursor.execute("SELECT * FROM movies")
+    myresult = mycursor.fetchall()
+    return render_template('ghibli.html', movies=myresult)
